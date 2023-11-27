42c59dad56ec7f59d3701d35de6455ee88be86f0
MariiaZubenko <mz19072004@gmail.com>
2023-11-27 21:40:25 +0200

    rgr

---
+++ bd_rgr/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace PostgreConsoleInteractorCS {
+    class Program {
+        static void Main(string[] args) {
+            DatabaseView view = new DatabaseView();
+            view.Process();
+        }
+    }
+}
